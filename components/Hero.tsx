5a5c4b64ef787c8822e05705c5132910093f436f
Parth Bhargava <parthbhargava1209@gmail.com>
2024-10-06 19:14:35 +1100

    Remove Vortex.tsx due to eslint build errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,6 @@ import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa';
-// import { Vortex } from './ui/Vortex';
 
 const Hero = () => {
   return (
@@ -19,7 +18,6 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      {/* <Vortex backgroundColor="black" className="absolute z-10"></Vortex> */}
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />

63799e1572d3e3407cf610ffcdba7dca13f2ab64
Parth Bhargava <parthbhargava1209@gmail.com>
2024-10-06 12:29:17 +1100

    Add Vortex.tsx from aceternity:

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa';
+// import { Vortex } from './ui/Vortex';
 
 const Hero = () => {
   return (
@@ -18,7 +19,7 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-
+      {/* <Vortex backgroundColor="black" className="absolute z-10"></Vortex> */}
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
@@ -30,10 +31,11 @@ const Hero = () => {
           </h2> */}
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Hi, I'm Parth."
+            words="Hi, my name is Parth."
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            A <span className="text-purple">software engineer</span> based in Sydney.
+            I&apos;m a <span className="text-purple">software engineer</span>{' '}
+            based in Sydney.
           </p>
 
           <a href="#about">

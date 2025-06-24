f52adbe4954527e727e5b2cf05fb85ed693c5c63
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-06-24 23:52:16 +1000

    Remove sentry stuff, change <img> to <Image />

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
-import SentryFeedbackWidget from '@/components/ui/sentry-feedback-widget';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -41,7 +40,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <SentryFeedbackWidget />
         </ThemeProvider>
       </body>
     </html>
--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { InfiniteMovingCards } from './ui/InfiniteMovingCards';
 import { companies, testimonials } from '@/data';
+import Image from 'next/image';
 
 const Clients = () => {
   return (
@@ -18,9 +19,22 @@ const Clients = () => {
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
+            // <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+            //   <img src={img} alt={name} className="md:w-10 w-5" />
+            //   <img src={nameImg} alt={name} className="md:w-24 w-20" />
+            // </div>
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
-              <img src={img} alt={name} className="md:w-10 w-5" />
-              <img src={nameImg} alt={name} className="md:w-24 w-20" />
+              <div className="relative md:w-10 w-5 aspect-square">
+                <Image src={img} alt={name} fill className="object-contain" />
+              </div>
+              <div className="relative md:w-24 w-20 aspect-[5/2]">
+                <Image
+                  src={nameImg}
+                  alt={name}
+                  fill
+                  className="object-contain"
+                />
+              </div>
             </div>
           ))}
         </div>
